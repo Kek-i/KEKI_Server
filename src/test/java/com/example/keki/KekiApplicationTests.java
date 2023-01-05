a624d651fed724997ae7bd4afa4edffd8f2c4dff
sojungpp <sophie2458@naver.com>
2023-01-05 14:13:25 +0900

    #1 feat: 프로젝트 초기 설정

---
+++ src/test/java/com/example/keki/KekiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KekiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
