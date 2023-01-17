cf76e7e853a03611aab4b4e7be96350919bdd61f
joonghyun <joongh@ewhain.net>
2023-01-18 00:45:37 +0900

    #7 feat: 디저트 상세 화면 조회 API

---
+++ src/main/java/com/codepatissier/keki/dessert/dto/GetDessertRes.java
@@ -0,0 +1,27 @@
+package com.codepatissier.keki.dessert.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetDessertRes {
+    private String nickname;
+    private String dessertName;
+    private int dessertPrice;
+    private String dessertDescription;
+    private List<Image> images;
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Image {
+        private Long postIdx;
+        private String postImgUrl;
+    }
+}
--- src/main/java/com/codepatissier/keki/dessert/repository/DessertRepository.java
+++ src/main/java/com/codepatissier/keki/dessert/repository/DessertRepository.java
@@ -2,15 +2,15 @@ package com.codepatissier.keki.dessert.repository;
 
 import com.codepatissier.keki.dessert.entity.Dessert;
 import com.codepatissier.keki.store.entity.Store;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface DessertRepository extends JpaRepository<Dessert, Long> {
-    List<Dessert> findByStoreOrderByDessertIdxDesc(Store store, Pageable page);
-    List<Dessert> findByStoreAndDessertIdxLessThanOrderByDessertIdxDesc(Store store, Long cursorIdx, Pageable page);
+    Page<Dessert> findByStoreOrderByDessertIdxDesc(Store store, Pageable page);
+    Page<Dessert> findByStoreAndDessertIdxLessThanOrderByDessertIdxDesc(Store store, Long cursorIdx, Pageable page);
     boolean existsByDessertIdxLessThan(Long dessertIdx);
 }
+
