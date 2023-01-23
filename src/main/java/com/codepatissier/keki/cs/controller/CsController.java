1d56891c01b66d5e97c395564437fa617845bef0
sojungpp <sophie2458@naver.com>
2023-01-23 14:05:56 +0900

    #67 feature: 공지사항 전체 조회 API 추가

--- src/main/java/com/codepatissier/keki/cs/controller/CsController.java
+++ src/main/java/com/codepatissier/keki/cs/controller/CsController.java
@@ -1,10 +1,14 @@
 package com.codepatissier.keki.cs.controller;
 
+import com.codepatissier.keki.common.BaseException;
+import com.codepatissier.keki.common.BaseResponse;
 import com.codepatissier.keki.cs.service.CsService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SecurityRequirement(name = "Bearer")
@@ -13,6 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/cs")
 @RequiredArgsConstructor
 public class CsController {
-    private CsService csService;
+    private final CsService csService;
+
+    // 공지사항 전체 조회
+    @GetMapping("/notice")
+    public BaseResponse<?> getNoticeList() {
+        try{
+            return new BaseResponse<>(csService.getNoticeList());
+        }catch (BaseException e){
+            return new BaseResponse<>(e.getStatus());
+        }
+    }
+
 
 }
--- src/main/java/com/codepatissier/keki/cs/service/CsService.java
+++ src/main/java/com/codepatissier/keki/cs/service/CsService.java
@@ -1,11 +1,29 @@
 package com.codepatissier.keki.cs.service;
 
+import com.codepatissier.keki.common.BaseException;
+import com.codepatissier.keki.cs.dto.GetNoticeListRes;
+import com.codepatissier.keki.cs.entity.Notice;
 import com.codepatissier.keki.cs.repository.NoticeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.codepatissier.keki.common.BaseResponseStatus.NO_NOTICE;
 
 @Service
 @RequiredArgsConstructor
 public class CsService {
     private final NoticeRepository noticeRepository;
+
+    // 공지사항 전체 조회
+    public List<GetNoticeListRes> getNoticeList() throws BaseException {
+        List<Notice> noticeList = noticeRepository.findAll(Sort.by(Sort.Direction.DESC, "createdDate"));
+        if(noticeList.isEmpty()) throw new BaseException(NO_NOTICE);
+        return noticeList.stream()
+                .map(notice -> new GetNoticeListRes(notice.getNoticeIdx(), notice.getNoticeTitle()))
+                .collect(Collectors.toList());
+    }
 }
