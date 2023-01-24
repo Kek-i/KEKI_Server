49d67defaaa9ae2960bb3446be283932f59b8691
Chang chae eun <90203250+chaerlo127@users.noreply.github.com>
2023-01-25 00:43:35 +0900

    #49 fix: 스펠링 변경

--- src/main/java/com/codepatissier/keki/TestController.java
+++ src/main/java/com/codepatissier/keki/TestController.java
@@ -14,6 +14,6 @@ public class TestController {
     @ResponseBody
     @GetMapping("/logs")
     public BaseResponse<String> testAPI() {
-        return new BaseResponse<>("Success test2");
+        return new BaseResponse<>("Success test");
     }
 }
