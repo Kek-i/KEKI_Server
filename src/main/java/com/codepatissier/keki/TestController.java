e2a05ba2916b7bedd53896d668e80f05b480ec79
chaerlo127 <monynony0203@gmail.com>
2023-01-24 22:41:22 +0900

    #1 feat: test api 생성

---
+++ src/main/java/com/codepatissier/keki/TestController.java
@@ -0,0 +1,19 @@
+package com.codepatissier.keki;
+
+import com.codepatissier.keki.common.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/test")
+@RequiredArgsConstructor
+public class TestController {
+    @ResponseBody
+    @GetMapping("/logs")
+    public BaseResponse<String> testAPI() {
+        return new BaseResponse<>("Succes test");
+    }
+}
