a92e7ef470f4efdab497d239503225716091d3e9
chaerlo127 <monynony0203@gmail.com>
2023-01-30 16:41:44 +0900

    #10 fix: 기념일 리스트 예외처리 제거

--- src/main/java/com/codepatissier/keki/calendar/dto/CalendarListRes.java
+++ src/main/java/com/codepatissier/keki/calendar/dto/CalendarListRes.java
@@ -1,17 +1,19 @@
 package com.codepatissier.keki.calendar.dto;
 
+import com.codepatissier.keki.common.EmptyStringToNullConverter;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Convert;
 import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-
+@Convert(converter = EmptyStringToNullConverter.class)
 public class CalendarListRes {
     private Long calendarIdx;
     private String title;
