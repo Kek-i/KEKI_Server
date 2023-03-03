0199474ab6fb34167c3fea62230bdfb99083acfd
chaerlo127 <monynony0203@gmail.com>
2023-03-03 23:01:00 +0900

    #10 fix: StringUtils 사용하여 공백 및 null 인 경우 에러 처리

--- src/main/java/com/codepatissier/keki/calendar/contoller/CalendarController.java
+++ src/main/java/com/codepatissier/keki/calendar/contoller/CalendarController.java
@@ -10,11 +10,10 @@ import com.codepatissier.keki.user.service.AuthService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
-// TODO STATUS 처리를 해줘야 함.
 
 @SecurityRequirement(name = "Bearer")
 @Tag(name = "calendars", description = "기념일 API")
@@ -30,9 +29,9 @@ public class CalendarController {
     @PostMapping("")
     public BaseResponse<String> createCalendar(@RequestBody CalendarReq calendarReq){
         try{
-            if(calendarReq.getTitle().equals("") || calendarReq.getTitle() == null) return new BaseResponse<>(BaseResponseStatus.NULL_TITLE);
-            if(calendarReq.getDate() == null || calendarReq.getDate().toString().equals("")) return new BaseResponse<>(BaseResponseStatus.NULL_DATE);
-            if(calendarReq.getKindOfCalendar() == null) return new BaseResponse<>(BaseResponseStatus.NULL_KIND_OF_CALENDARS);
+            if(!StringUtils.hasText(calendarReq.getTitle())) return new BaseResponse<>(BaseResponseStatus.NULL_TITLE);
+            if(!StringUtils.hasText(calendarReq.getDate().toString())) return new BaseResponse<>(BaseResponseStatus.NULL_DATE);
+            if(!StringUtils.hasText(calendarReq.getKindOfCalendar())) return new BaseResponse<>(BaseResponseStatus.NULL_KIND_OF_CALENDARS);
 
             this.calendarService.createCalendar(this.authService.getUserIdx(), calendarReq);
             return new BaseResponse<>(BaseResponseStatus.SUCCESS);
@@ -132,7 +131,7 @@ public class CalendarController {
     @PostMapping("/categories")
     public BaseResponse<String> createTag(@RequestBody CalendarHashTag tag){
         try{
-            if(tag.getCalendarHashTag() == null) return new BaseResponse<>(BaseResponseStatus.NULL_TAG);
+            if(!StringUtils.hasText(tag.getCalendarHashTag())) return new BaseResponse<>(BaseResponseStatus.NULL_TAG);
             this.calendarService.createTag(tag);
             return new BaseResponse<>(BaseResponseStatus.SUCCESS);
         }catch (BaseException e){
@@ -145,7 +144,7 @@ public class CalendarController {
     @PatchMapping("/categories")
     public BaseResponse<String> patchTag(@RequestBody TagStatus tag){
         try{
-            if(tag.getCalendarHashTag() == null) return new BaseResponse<>(BaseResponseStatus.NULL_TAG);
+            if(!StringUtils.hasText(tag.getCalendarHashTag())) return new BaseResponse<>(BaseResponseStatus.NULL_TAG);
             this.calendarService.patchTag(tag);
 
             return new BaseResponse<>(BaseResponseStatus.SUCCESS);
