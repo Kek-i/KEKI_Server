7a7184b20de76f8a071735e65e2a6add26612be1
chaerlo127 <monynony0203@gmail.com>
2023-04-12 17:49:32 +0900

    #199 feat: StompConfig 생성

---
+++ src/main/java/com/codepatissier/keki/common/config/StompConfig.java
@@ -0,0 +1,24 @@
+package com.codepatissier.keki.common.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@Slf4j
+public class StompConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/keki/chat").setAllowedOrigins("*");
+    }
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.setApplicationDestinationPrefixes("/pub");
+        config.enableSimpleBroker("/sub");
+    }
+}
+
